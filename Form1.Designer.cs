40a27aaf2af30b3dc2fbaa8b78297b826be168ec
tial00 <tical0000@gmail.com>
2023-04-03 10:23:58 +0900

    초기 커밋.

---
+++ Form1.Designer.cs
@@ -0,0 +1,253 @@
+﻿namespace ML.winform
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.model_output = new System.Windows.Forms.Label();
+            this.sepal_length_input = new System.Windows.Forms.TrackBar();
+            this.sepal_width_input = new System.Windows.Forms.TrackBar();
+            this.petal_length_input = new System.Windows.Forms.TrackBar();
+            this.petal_width_input = new System.Windows.Forms.TrackBar();
+            this.result = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.sepal_length_input)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.sepal_width_input)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.petal_length_input)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.petal_width_input)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("맑은 고딕", 20F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label1.Location = new System.Drawing.Point(5, 5);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(445, 37);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Data Analysis App - Windows ML";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("맑은 고딕", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label2.Location = new System.Drawing.Point(5, 50);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(155, 21);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Provide the input :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("맑은 고딕", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label3.Location = new System.Drawing.Point(5, 180);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(68, 21);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Output:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("맑은 고딕", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label4.Location = new System.Drawing.Point(5, 210);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(326, 19);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Based on the information provided, the Iris type is:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("맑은 고딕", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label5.Location = new System.Drawing.Point(5, 80);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(258, 19);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "sepal length in mm [range of 10 - 100]:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("맑은 고딕", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label6.Location = new System.Drawing.Point(5, 105);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(245, 19);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "sepal width in mm [range of 10 - 80]:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("맑은 고딕", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label7.Location = new System.Drawing.Point(5, 130);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(249, 19);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "petal length in mm [range of 5 - 100]:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("맑은 고딕", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label8.Location = new System.Drawing.Point(5, 155);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(237, 19);
+            this.label8.TabIndex = 7;
+            this.label8.Text = "sepal width in mm [range of 1 - 50]:";
+            // 
+            // model_output
+            // 
+            this.model_output.AutoSize = true;
+            this.model_output.Font = new System.Drawing.Font("맑은 고딕", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.model_output.Location = new System.Drawing.Point(340, 210);
+            this.model_output.Name = "model_output";
+            this.model_output.Size = new System.Drawing.Size(95, 19);
+            this.model_output.TabIndex = 8;
+            this.model_output.Text = "Model output";
+            // 
+            // sepal_length_input
+            // 
+            this.sepal_length_input.AutoSize = false;
+            this.sepal_length_input.Location = new System.Drawing.Point(270, 80);
+            this.sepal_length_input.Maximum = 100;
+            this.sepal_length_input.Minimum = 10;
+            this.sepal_length_input.Name = "sepal_length_input";
+            this.sepal_length_input.Size = new System.Drawing.Size(280, 20);
+            this.sepal_length_input.TabIndex = 9;
+            this.sepal_length_input.TickStyle = System.Windows.Forms.TickStyle.None;
+            this.sepal_length_input.Value = 10;
+            this.sepal_length_input.ValueChanged += new System.EventHandler(this.input_ValueChanged);
+            // 
+            // sepal_width_input
+            // 
+            this.sepal_width_input.AutoSize = false;
+            this.sepal_width_input.Location = new System.Drawing.Point(270, 105);
+            this.sepal_width_input.Maximum = 80;
+            this.sepal_width_input.Minimum = 10;
+            this.sepal_width_input.Name = "sepal_width_input";
+            this.sepal_width_input.Size = new System.Drawing.Size(280, 20);
+            this.sepal_width_input.TabIndex = 10;
+            this.sepal_width_input.TickStyle = System.Windows.Forms.TickStyle.None;
+            this.sepal_width_input.Value = 10;
+            this.sepal_width_input.ValueChanged += new System.EventHandler(this.input_ValueChanged);
+            // 
+            // petal_length_input
+            // 
+            this.petal_length_input.AutoSize = false;
+            this.petal_length_input.Location = new System.Drawing.Point(270, 130);
+            this.petal_length_input.Maximum = 100;
+            this.petal_length_input.Minimum = 5;
+            this.petal_length_input.Name = "petal_length_input";
+            this.petal_length_input.Size = new System.Drawing.Size(280, 20);
+            this.petal_length_input.TabIndex = 11;
+            this.petal_length_input.TickStyle = System.Windows.Forms.TickStyle.None;
+            this.petal_length_input.Value = 5;
+            this.petal_length_input.ValueChanged += new System.EventHandler(this.input_ValueChanged);
+            // 
+            // petal_width_input
+            // 
+            this.petal_width_input.AutoSize = false;
+            this.petal_width_input.Location = new System.Drawing.Point(270, 155);
+            this.petal_width_input.Maximum = 50;
+            this.petal_width_input.Minimum = 1;
+            this.petal_width_input.Name = "petal_width_input";
+            this.petal_width_input.Size = new System.Drawing.Size(280, 20);
+            this.petal_width_input.TabIndex = 12;
+            this.petal_width_input.TickStyle = System.Windows.Forms.TickStyle.None;
+            this.petal_width_input.Value = 1;
+            this.petal_width_input.ValueChanged += new System.EventHandler(this.input_ValueChanged);
+            // 
+            // result
+            // 
+            this.result.AutoSize = true;
+            this.result.Font = new System.Drawing.Font("맑은 고딕", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.result.Location = new System.Drawing.Point(5, 235);
+            this.result.Name = "result";
+            this.result.Size = new System.Drawing.Size(0, 19);
+            this.result.TabIndex = 13;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(553, 445);
+            this.Controls.Add(this.result);
+            this.Controls.Add(this.petal_width_input);
+            this.Controls.Add(this.petal_length_input);
+            this.Controls.Add(this.sepal_width_input);
+            this.Controls.Add(this.sepal_length_input);
+            this.Controls.Add(this.model_output);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("맑은 고딕", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.sepal_length_input)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.sepal_width_input)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.petal_length_input)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.petal_width_input)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label model_output;
+        private System.Windows.Forms.TrackBar sepal_length_input;
+        private System.Windows.Forms.TrackBar sepal_width_input;
+        private System.Windows.Forms.TrackBar petal_length_input;
+        private System.Windows.Forms.TrackBar petal_width_input;
+        private System.Windows.Forms.Label result;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,187 @@
+﻿using Microsoft.ML;
+using Microsoft.ML.Data;
+using Microsoft.ML.Transforms.Onnx;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ML.winform
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            this.Load += Form1_Load;
+        }
+
+        MLContext mlContext;
+        OnnxScoringEstimator onnxPredictionPipeline;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            mlContext = new MLContext();
+
+            string onnxPath = Path.Combine(Application.StartupPath, @"Assets\Network.onnx");
+            onnxPredictionPipeline = mlContext.Transforms.ApplyOnnxModel(onnxPath);
+        }
+
+        public class OnnxInput
+        {
+            private float _sepal_length = 1.0f;
+            [ColumnName("Sepal_Length")]
+            public float Sepal_Length
+            {
+                get
+                {
+                    return _sepal_length;
+                }
+                set
+                {
+                    // validate range [1,10]
+                    if (value >= 1 && value <= 10)
+                    {
+                        _sepal_length = value;
+                    }
+                }
+            }
+
+            private float _sepal_width = 1.0f;
+            [ColumnName("Sepal_Width")]
+            public float Sepal_Width
+            {
+                get
+                {
+                    return _sepal_width;
+                }
+                set
+                {
+                    // validate range [1, 8]
+                    if (value >= 1 && value <= 8)
+                    {
+                        _sepal_width = value;
+                    }
+                }
+            }
+
+            private float _petal_length = 0.5f;
+            [ColumnName("Petal_Length")]
+            public float Petal_Length
+            {
+                get
+                {
+                    return _petal_length;
+                }
+                set
+                {
+                    // validate range [0.5, 10]
+                    if (value >= 0.5 && value <= 10)
+                    {
+                        _petal_length = value;
+                    }
+                }
+            }
+
+            private float _petal_width = 0.1f;
+            [ColumnName("Petal_Width")]
+            public float Petal_Width
+            {
+                get
+                {
+                    return _petal_width;
+                }
+                set
+                {
+                    // validate range [0.1, 5]
+                    if (value >= 0.1 && value <= 5)
+                    {
+                        _petal_width = value;
+                    }
+                }
+            }
+        }
+
+        public class OnnxOutput
+        {
+            [ColumnName("output")]
+            public string PredictedIris { get; set; }
+        }
+        private string[] _labels = { "Iris-setosa", "Iris-versiocolor", "Iris-verginica" };
+
+        private void input_ValueChanged(object sender, EventArgs e)
+        {
+            
+            if (mlContext != null) 
+            {
+                var input = new OnnxInput
+                {
+                    Sepal_Length = (float)sepal_length_input.Value / 10.0f,
+                    Sepal_Width = (float)sepal_width_input.Value / 10.0f,
+                    Petal_Length = (float)petal_length_input.Value / 10.0f,
+                    Petal_Width = (float)petal_length_input.Value / 10.0f
+                };
+
+                var data = GetInputData(input, mlContext);
+                var transformedValues = onnxPredictionPipeline.Fit(data).Transform(data);
+                var predictions = mlContext.Data.CreateEnumerable<Prediction>(
+                                transformedValues, reuseRowObject: false);
+
+                string strResult = "";
+                
+                int max_index = 0;
+                float max_value = 0;
+     
+                foreach (var item in predictions)
+                {
+                    int num = 0;
+                    foreach (var score in item.output.Take(outputSize))
+                    {
+                        if (score > max_value)
+                        {
+                            max_value = score;
+                            max_index = num;
+                        }
+                        strResult += $"Case #{num+1} {_labels.ElementAt(num)} Score: {score}\n";
+                        num++;
+                    }
+                }
+
+                result.Text = strResult;
+                model_output.Text = _labels.ElementAt(max_index);
+            }
+        }
+
+        private const int inputSize = 1 * 4;
+        public class TensorData
+        {
+            [VectorType(inputSize)]
+            public float[] input { get; set; }
+        }
+        public static IDataView GetInputData(OnnxInput _input, MLContext context)
+        {
+            float[] data = new float[4];
+            data[0] = _input.Sepal_Length;
+            data[1] = _input.Sepal_Width;
+            data[2] = _input.Petal_Length;
+            data[3] = _input.Petal_Width;
+            return context.Data.LoadFromEnumerable(new TensorData[] {
+                new TensorData { input = data }
+            });
+        }
+
+        private const int outputSize = 1 * 3;
+        class Prediction
+        {
+            [VectorType(outputSize)]
+            public float[] output { get; set;}
+        }
+
+    }
+}
